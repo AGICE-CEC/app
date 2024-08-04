027ceeb939c8960a3d0e0ac133af7f66c436dc4c
cor22982 <cor22982@uvg.edu.gt>
2024-08-03 22:42:13 -0600

    feat: Customize emaiil input and add validators

--- agice_app/lib/screens/registration_screen.dart
+++ agice_app/lib/screens/registration_screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class RegistrationScreen extends StatelessWidget {
+class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({super.key});
+
+  @override
+  State<RegistrationScreen> createState() => _RegistrationScreen();
+}
+
+class _RegistrationScreen extends State<RegistrationScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -9,19 +17,43 @@ class RegistrationScreen extends StatelessWidget {
       appBar: AppBar(title: const Text('Registration Form')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(),
-            TextField(),
-            SizedBox(height: 20),
-            OutlinedButton(
-              child: Text("Explore!"),
-              onPressed: () {
-                Navigator.of(context).pushReplacementNamed('/main');
-              },
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                  hintText: 'Ingrese su email',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, ingrese su email';
+                  }
+
+                  final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+                  if (!emailRegex.hasMatch(value)) {
+                    return 'Ingrese un email válido';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              OutlinedButton(
+                child: Text("Explore!"),
+                onPressed: () {
+                  if (_formKey.currentState?.validate() ?? false) {
+                    Navigator.of(context).pushReplacementNamed('/main');
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
