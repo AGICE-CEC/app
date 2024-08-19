44f2cf1ac56ee78adea1ae2dd100b9d2be078564
cor22982 <cor22982@uvg.edu.gt>
2024-08-18 19:16:40 -0600

    feat: Refactor main.dart

--- agice_app/lib/main.dart
+++ agice_app/lib/main.dart
@@ -5,25 +5,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding
-      .ensureInitialized(); // Asegúrate de que Flutter esté inicializado
+  WidgetsFlutterBinding.ensureInitialized();
 
-  try {
-    // Inicialización de Firebase
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-    // Inicialización de OneSignal
-    OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
-    OneSignal.initialize("0b71d6cd-e5da-4c6f-abde-cd8456476acf");
-    OneSignal.Notifications.requestPermission(true); // Elimina await aquí
+  OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+  OneSignal.initialize("0b71d6cd-e5da-4c6f-abde-cd8456476acf");
+  OneSignal.Notifications.requestPermission(true);
 
-    // Inicia la aplicación
-    runApp(MyApp());
-  } catch (e, stacktrace) {
-    // Manejo de errores
-    print("Error al inicializar Firebase o OneSignal: $e");
-    print("Stacktrace: $stacktrace");
-  }
+  runApp(MyApp());
 }
