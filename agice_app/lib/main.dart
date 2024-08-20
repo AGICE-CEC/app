ab36b6f0b434bcb1e545b05d3c3482cb9e1badb9
cor22982 <cor22982@uvg.edu.gt>
2024-08-20 13:49:07 -0600

    feat: Complete notifications for android

--- agice_app/lib/main.dart
+++ agice_app/lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:agice_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'app/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+  OneSignal.initialize("1cb74d81-5e43-411f-bb8a-22828e6d385a");
+  OneSignal.Notifications.requestPermission(true);
   runApp(MyApp());
 }
