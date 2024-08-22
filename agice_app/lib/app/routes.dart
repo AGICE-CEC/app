ee6d101f7a3da5f578a182d70803a9411a9e8b2d
Cristian de León <109119188+Cristian633@users.noreply.github.com>
2024-08-21 22:54:49 -0600

    integracion del mapa

--- agice_app/lib/app/routes.dart
+++ agice_app/lib/app/routes.dart
@@ -3,6 +3,8 @@ import '../screens/speakers_screen/main_speaker_screen.dart';
 import '../screens/welcome_screen.dart';
 import '../screens/registration_screen.dart';
 import '../screens/main_screen/main_screen.dart';
+import '../screens/map_screen.dart';
+
 
 class AppRoutes {
   static final routes = {
@@ -10,5 +12,6 @@ class AppRoutes {
     '/registration': (context) => const RegistrationScreen(),
     '/main': (context) => MainScreen(),
     '/speakers': (context) => MainSpeakerScreen(),
+    '/mapa': (context) => MapScreen(),
   };
 }
