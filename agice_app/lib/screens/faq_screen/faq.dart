56075e6200da56b9a4d8f1b377ba5af824ad6ced
MixqMo <89754808+MixqMo@users.noreply.github.com>
2024-08-31 18:39:23 -0600

    EndPoint para FAQ
    Se añade la funcionalidad para usar el endpoint en la página.

--- agice_app/lib/screens/faq_screen/faq.dart
+++ agice_app/lib/screens/faq_screen/faq.dart
@@ -10,8 +10,6 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class FAQPage extends StatefulWidget {
-  const FAQPage({super.key});
-
   @override
   _FAQPageState createState() => _FAQPageState();
 }
@@ -30,15 +28,18 @@ class _FAQPageState extends State<FAQPage> {
       const String url = "https://server-production-2c4b.up.railway.app/faqs";
       final response = await http.get(Uri.parse(url));
 
+      // Verificación del código de estado de la respuesta
       if (response.statusCode == 200) {
-        print("Datos recibidos: ${response.body}");
+        print(
+            "Datos recibidos: ${response.body}"); // Log para confirmar los datos recibidos
         return jsonDecode(response.body);
       } else {
-        print("Error al cargar FAQs: ${response.statusCode}");
+        print(
+            "Error al cargar FAQs: ${response.statusCode}"); // Log en caso de error en la solicitud
         throw Exception('Failed to load FAQs');
       }
     } catch (e) {
-      print("Excepción al cargar FAQs: $e");
+      print("Excepción al cargar FAQs: $e"); // Log de la excepción
       throw Exception('Failed to load FAQs');
     }
   }
@@ -46,99 +47,96 @@ class _FAQPageState extends State<FAQPage> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text(
-                'Preguntas Frecuentes',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 25,
-                ),
-              )
-            ],
-          ),
-          backgroundColor: const Color(0xFF1a1a1a),
-          bottom: const TabBar(
-            labelColor: Colors.white,
-            unselectedLabelColor: Colors.grey,
-            indicatorColor: Colors.white,
-            tabs: [
-              Tab(text: 'Español'),
-              Tab(text: 'English'),
-            ],
-          ),
-        ),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color(0xFF232323),
-                Color(0xFF4a4a4a),
-              ],
-            ),
-          ),
-          child: FutureBuilder<List<dynamic>>(
-            future: faqs,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                print("Cargando datos...");
-                return const Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                print("Error en snapshot: ${snapshot.error}");
-                return Center(child: Text('Error: ${snapshot.error}'));
-              } else if (snapshot.hasData) {
-                print("Datos cargados correctamente.");
-                return TabBarView(
-                  children: [
-                    ListView.builder(
-                      itemCount: snapshot.data?.length,
-                      itemBuilder: (context, index) {
-                        final faq = snapshot.data?[index];
-                        return Container(
-                          margin: const EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.grey, width: 1.0),
-                            borderRadius: BorderRadius.circular(8.0),
-                            color: const Color(0xFF848484),
-                          ),
-                          child: ExpansionTile(
-                            title: Text(
-                              faq['question'], // Utiliza la clave 'question'
-                              style: const TextStyle(color: Colors.white),
-                            ),
-                            iconColor: Colors.white70,
-                            collapsedIconColor: Colors.white,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: Text(
-                                  faq['answer'], // Utiliza la clave 'answer'
-                                  style: const TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            ],
-                          ),
-                        );
-                      },
+        length: 2,
+        child: Scaffold(
+            appBar: AppBar(
+              title: const Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Preguntas Frecuentes',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
                     ),
-                    const Center(
-                        child: Text('English FAQs not implemented yet')),
-                  ],
-                );
-              } else {
-                print("No FAQs found.");
-                return const Center(child: Text('No FAQs found'));
-              }
-            },
-          ),
-        ),
-      ),
-    );
+                  )
+                ],
+              ),
+              backgroundColor: const Color(0xFF1a1a1a),
+              bottom: const TabBar(
+                labelColor: Colors.white,
+                unselectedLabelColor: Colors.grey,
+                indicatorColor: Colors.white,
+                tabs: [
+                  Tab(text: 'Español'),
+                  Tab(text: 'English'),
+                ],
+              ),
+            ),
+            body: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Color(0xFF232323),
+                      Color(0xFF4a4a4a),
+                    ],
+                  ),
+                ),
+                child: FutureBuilder<List<dynamic>>(
+                    future: faqs,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        print("Cargando datos...");
+                        return const Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasError) {
+                        print("Error en snapshot: ${snapshot.error}");
+                        return Center(child: Text('Error: ${snapshot.error}'));
+                      } else if (snapshot.hasData) {
+                        print("Datos cargados correctamente.");
+                        return TabBarView(children: [
+                          ListView(children: [
+                            ListView.builder(
+                                itemCount: snapshot.data?.length,
+                                itemBuilder: (context, index) {
+                                  final faq = snapshot.data?[index];
+
+                                  return Container(
+                                    margin: const EdgeInsets.all(8.0),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                          color: Colors.grey, width: 1.0),
+                                      borderRadius: BorderRadius.circular(8.0),
+                                      color: const Color(0xFF848484),
+                                    ),
+                                    child: ExpansionTile(
+                                      title: Text(
+                                        faq['question'],
+                                        style: const TextStyle(
+                                            color: Colors.white),
+                                      ),
+                                      iconColor: Colors.white70,
+                                      collapsedIconColor: Colors.white,
+                                      children: <Widget>[
+                                        Padding(
+                                          padding: const EdgeInsets.all(16.0),
+                                          child: Text(
+                                            faq['answer'],
+                                            style: const TextStyle(
+                                                color: Colors.white),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                })
+                          ])
+                        ]);
+                      } else {
+                        print("No FAQs found.");
+                        return const Center(child: Text('No FAQs found'));
+                      }
+                    }))));
   }
 }
