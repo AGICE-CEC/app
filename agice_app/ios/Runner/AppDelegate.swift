8bdcf481f6c1f55f4500e612fb9190e21ed3e7f9
cor22982 <cor22982@uvg.edu.gt>
2024-08-31 18:29:26 -0600

    Feature ios notifications

--- agice_app/ios/Runner/AppDelegate.swift
+++ agice_app/ios/Runner/AppDelegate.swift
@@ -1,13 +1,30 @@
 import Flutter
 import UIKit
+import OneSignalFramework
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+[UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+  // Remove this method to stop OneSignal Debugging
+  OneSignal.Debug.setLogLevel(.LL_VERBOSE)
+
+  // OneSignal initialization
+  OneSignal.initialize("1cb74d81-5e43-411f-bb8a-22828e6d385a", withLaunchOptions: launchOptions)
+
+  // requestPermission will show the native iOS notification permission prompt.
+  // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  OneSignal.Notifications.requestPermission({ accepted in
+    print("User accepted notifications: (accepted)")
+  }, fallbackToSettings: true)
+
+  // Login your customer with externalId
+  // OneSignal.login("EXTERNAL_ID")
+
+  return true
+}
+
+// Remaining contents of your AppDelegate Class...
 }
